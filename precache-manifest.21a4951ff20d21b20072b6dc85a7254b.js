c15b4a4af8082f77790ed8979584b8c9f11be0ea
nikhilbrij <nikhilbrijpuriya@gmail.com>
2019-12-18 12:02:09 +0530

    Updates

---
+++ precache-manifest.21a4951ff20d21b20072b6dc85a7254b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e158735968e2e52da232339d8f297342",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "25f6d2942440c1f36678",
+    "url": "/monster-rolodex/static/css/main.9c370d53.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25f6d2942440c1f36678",
+    "url": "/monster-rolodex/static/js/main.bcbb77ba.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
